9c33a83b288c91be819b38bdcdb5bf7655b4b328
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-07-08 17:33:40 -0500

    Add missing imports pt2

--- feature/settings/src/main/kotlin/com/stash/feature/settings/SettingsPlaybackScreen.kt
+++ feature/settings/src/main/kotlin/com/stash/feature/settings/SettingsPlaybackScreen.kt
@@ -1,13 +1,24 @@
 package com.stash.feature.settings
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.stash.core.common.constants.StashConstants
+import com.stash.feature.settings.components.SettingsRowPadH
+import com.stash.feature.settings.components.SettingsRowPadV
 import com.stash.feature.settings.components.SettingsGroupCard
 import com.stash.feature.settings.components.SettingsScaffold
 import com.stash.feature.settings.components.SettingsSectionLabel
