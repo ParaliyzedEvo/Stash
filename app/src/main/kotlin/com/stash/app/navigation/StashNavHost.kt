886707077b82c827f7d471fcca3dba26bcd2f2e2
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-09-05 00:41:37 -0500

    Add info links [skip test]

--- app/src/main/kotlin/com/stash/app/navigation/StashNavHost.kt
+++ app/src/main/kotlin/com/stash/app/navigation/StashNavHost.kt
@@ -209,6 +209,7 @@ fun StashNavHost(
                 onOpenAppearance = { navController.navigate(SettingsAppearanceRoute) },
                 onOpenAbout = { navController.navigate(SettingsAboutRoute) },
                 onDonate = { runCatching { uriHandler.openUri("https://ko-fi.com/rawnald") } },
+                onDonateCoDev = { runCatching { uriHandler.openUri("https://www.paypal.com/paypalme/Paraliyzedevo") } },
                 onStar = { runCatching { uriHandler.openUri("https://github.com/rawnaldclark/Stash") } },
             )
         }
