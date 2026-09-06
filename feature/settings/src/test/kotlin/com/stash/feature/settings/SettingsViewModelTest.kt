ce2b7d37b5af60c1de476ba5b664aba9cd825318
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-09-05 20:53:04 -0500

    Fix unit tests

--- feature/settings/src/test/kotlin/com/stash/feature/settings/SettingsViewModelTest.kt
+++ feature/settings/src/test/kotlin/com/stash/feature/settings/SettingsViewModelTest.kt
@@ -5,6 +5,7 @@ import com.stash.data.download.files.LibrarySizeHolder
 import com.stash.data.download.lossless.LosslessAvailability
 import com.stash.data.download.lossless.LosslessSourcePreferences
 import com.stash.data.download.lossless.qbdlx.QbdlxCredentialStore
+import com.stash.core.data.discord.DiscordRpcCoordinator
 import com.stash.data.download.lossless.relay.LosslessRelayClient
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -90,6 +91,7 @@ class SettingsViewModelTest {
         listenSinkCoordinator = mockk(relaxed = true),
         listenSubmissionDao = mockk(relaxed = true),
         relayClient = relayClient,
+        discordRpcCoordinator = mockk(relaxed = true),
     )
 
     @Test fun `refreshStorageUsage requests a fresh filesystem calculation`() {
